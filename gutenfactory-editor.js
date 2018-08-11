00c16aad49cea15b9c77bd4d3993de8b3384e7d9
Arūnas Liuiza <aliuiza@kayak.com>
2018-08-11 14:07:49 -0400

    some work on output

--- gutenfactory-editor.js
+++ gutenfactory-editor.js
@@ -3,93 +3,129 @@
 	var el = wp.element.createElement;
 	var __ = wp.i18n.__;
 	Object.keys( blocks ).forEach( function( block_name ) {
-		var block = blocks[block_name];
-		var block_attributes = {};
-		var block_inputs = [];
+		var block 						= blocks[block_name];
+		var block_attributes 	= {};
+		var block_inputs 			= [];
+		var inspector_inputs 	= [];
 		Object.keys( block.fields ).forEach( function( field_name ) {
-			var field = block.fields[field_name];
+			var field = block.fields[ field_name ];
 			switch ( field.control ) {
 				case 'RichText' :
 					var input = {
-						'type'	: 'RichText',
+						'type'	: wp.editor.RichText,
 						'name'  : field_name,
+						'default' : field.default,
+						'props' 	: field.props,
+
 						'props' : {
-							'tagName' : 'p',
+							'format' : 'string',
+							'tagName'	: 'div',
+							'className' : 'content',
 						},
 					};
 					var attribute = {
-						type: 'array',
-						source: 'children',
-						selector: 'p',
+						type: 'string',
 					};
 				break;
-				case 'MediaLibrary' :
+				// case 'MediaUpload' :
+				// 	var input = {
+				// 		'type'	: wp.editor.MediaUpload,
+				// 		'name'  : field_name,
+				// 		'props' : {
+				// 			'render' : function( open ) { return el( wp.components.Button, { onClick: open.open }, 'Upload' ) },
+				// 		// 	'accept' : 'image/*',
+				// 		},
+				// 		// 'content' : 'Upload',
+				// 	};
+				// 	var attribute = {
+				// 	 type: 'object',
+				// 	};
+				// break;
+				default :
 					var input = {
-						'type'	: 'RichText',
-						'name'  : field_name,
-						'props' : {
-							'tagName' : 'p',
-						},
+						'type'		: wp.components[ field.control ],
+						'name'  	: field_name,
+						'default' : field.default,
+						'props' 	: field.props,
 					};
 					var attribute = {
-					 type: 'string',
+					 type: field.data_type,
 					};
+				break;
 			}
-			block_inputs.push(
-				input
-			);
+			switch( field.position ) {
+				case 'inline' :
+					block_inputs.push( input );
+				break;
+				case 'inspector' :
+					inspector_inputs.push( input );
+				break;
+			}
 			block_attributes[field_name] = attribute;
 		} );
+		console.log( block_attributes );
 		registerBlockType( block_name, {
 			title: block.title,
 			category: block.category,
 			attributes: block_attributes,
 			edit: function( props ) {
-				var ret = [];
-				block_inputs.forEach( function( input ) {
-					input.props['value'] = props.attributes[block_name],
+				var setup_input = function ( input, props ) {
+					input.props['value'] = undefined === props.attributes[ input.name ] ? input.default : props.attributes[ input.name ],
 					input.props['onChange'] = function( new_value ){
+						console.log( new_value );
 						var n = {};
-						n[input.name] = new_value
+						n[input.name] = new_value;
 						props.setAttributes( n );
-					},
-					ret.push( el(
-						wp.editor[ input.type ],
-						input.props
-					) );
+					};
+					console.log( input );
+					return el(
+						input.type,
+						input.props,
+						input.content,
+					);
+				}
+				var block_ret = [];
+				block_inputs.forEach( function( input ) {
+					block_ret.push( setup_input( input, props ) );
 				} );
+				block_ret = el(
+					'div',
+					{ 'className' : 'gutenfactory-settings' },
+					block_ret
+				);
 				var inspector = [];
-				inspector.push( el(
+				inspector_inputs.forEach( function( input ) {
+					var inspector_el = setup_input( input, props );
+					inspector.push(
+						el(
+						  wp.components.PanelRow,
+						  null,
+						  inspector_el
+					  )
+				  );
+				} );
+				var inspector_ret = [];
+				inspector_ret.push( el(
 					wp.editor.InspectorControls,
 					null,
-					[
-						el(
-							wp.components.PanelBody,
-							null,
-							[
-								el(
-									wp.components.PanelRow,
-									null,
-									[
-										el(
-											wp.components.TextareaControl,
-											null
-										)
-									]
-								)
-							]
-						)
-					]
+					el(
+						wp.components.PanelBody,
+						null,
+						inspector
+					)
 				) );
-				return inspector;
+				var render_ret = el(
+					wp.components.ServerSideRender,
+					{
+						'block'				: block_name,
+						'attributes' 	: props.attributes,
+					}
+				);
+				return [block_ret, inspector_ret ];
 			},
 
 			save: function( props ) {
-				return wp.element.createElement(
-	        'div',
-	        null,
-	        'boo'
-    		);
+				return false;
 			}
 		} );
 	});
--- gutenfactory.php
+++ gutenfactory.php
@@ -5,43 +5,103 @@
  */
 
 $gutenfactory_blocks = [
-  'namespace/name' => [
-    'title' => __( 'My Block', 'gutenfactory' ),
-    'category' => 'common',
-    'fields'  => [
-      'content' => [
-        'control' => 'RichText',
-        'display' => 'div',
-      ],
-      'background' => [
-        'control' => 'MediaLibrary',
-        'display' => 'css',
-      ]
-    ],
-    'style' =>   plugins_url( 'my-block.css', __FILE__ ),
-    'editor_style' =>   plugins_url( 'my-block-editor.css', __FILE__ ),
-  ],
   'namespace/name2' => [
     'title' => __( 'My Block2', 'gutenfactory' ),
     'category' => 'common',
+    // 'type' => //static|dynamic
     'fields'  => [
+      'heading' => [
+        'control' => 'RichText',
+        'position'=> 'inline',
+        'data_type' => 'string',
+        'props' => [
+          'format' => 'string',
+          'tagName' => 'h2',
+          'className' => 'heading',
+          'placeholder' => 'coool heading',
+        ],
+      ],
       'content' => [
         'control' => 'RichText',
-        'display' => 'div',
+        'position'=> 'inline',
+        'data_type' => 'string',
+        'props' => [
+          'format' => 'string',
+          'tagName' => 'div',
+          'className' => 'content',
+          'placehoder' => 'cool text',
+        ],
       ],
-      // 'background' => [
-      //   'control' => 'MediaLibrary',
-      //   'display' => 'css',
-      // ]
+      // 'content2' => [
+      //   'control' => 'MediaUpload',
+      //   'position'=> 'inline',
+      // ],
+      // 'some-more' => [
+      //   'control' => 'TextareaControl',
+      //   'position'=> 'inline',
+      // ],
+      'description' => [
+        'control'   => 'TextareaControl',
+        'position'  => 'inline',
+        'label'     => __( 'My Block Description', 'gutenfactory' ),
+        'help'      => __( 'Write something here, too.', 'gutenfactory' ),
+      ],
+      'class' => [
+        'control'   => 'TextControl',
+        'position'  => 'inspector',
+        'default'   => 'coool',
+        'label'     => __( 'My Block Class', 'gutenfactory' ),
+        'help'      => __( 'Write something here', 'gutenfactory' ),
+        // 'props'     => [
+        //   'type'  => 'number',
+        //   'min'   => 10,
+        //   'step'  => 10,
+        // ],
+      ],
+      'range' => [
+        'control'   => 'RangeControl',
+        'position'  => 'inline',
+        'label'     => __( 'My Range', 'gutenfactory' ),
+        'default'   => 50,
+        'props'     => [
+          'min'   => 10,
+          'max'   => 100,
+          'step'  => 10,
+        ],
+      ]
+
     ],
     'callback' => 'my_awesome_block',
     'style' =>   plugins_url( 'my-block.css', __FILE__ ),
     'editor_style' =>   plugins_url( 'my-block-editor.css', __FILE__ ),
   ],
 ];
-function my_awesome_block( $fields ) {
-  $output = '<div style="background-image:url('.$fields['background'].');">'.$fields['content'].'</div>';
+function my_awesome_block( $fields, $content ) {
+  $output = '';
+  // foreach ( $fields['content'] as $cont ) {
+  $output .= '<h2 class="heading">' . $fields['heading'] . '</h2>';
+  $output .= '<div class="content">' . $fields['content'] . '</div>';
+  // $output .= var_export( $fields, true );
   return $output;
+}
+function gutenfactory_prefill( $attributes, $name ) {
+  if ( ! isset( $attributes['data_type'] ) ) {
+    $attributes['data_type'] = 'string';
+  }
+  if ( ! isset( $attributes['content'] ) ) {
+    $attributes['content'] = '';
+  }
+  if ( ! isset( $attributes['label'] ) ) {
+    $attributes['label'] = '';
+  }
+  if ( ! isset( $attributes['props'] ) ) {
+    $attributes['props'] = [];
+  }
+  $attributes['props']['label'] = $attributes['label'];
+  if ( isset( $attributes['help'] ) ) {
+    $attributes['props']['help'] = $attributes['help'];
+  }
+  return $attributes;
 }
 
 function gutenfactory_init() {
@@ -64,6 +124,9 @@ function gutenfactory_run() {
     $version
   );
   foreach ( $gutenfactory_blocks as $name => $attributes ) {
+    foreach ( $attributes['fields'] as $attr_name => $attr ) {
+      $attributes['fields'][ $attr_name ] = gutenfactory_prefill( $attr, $attr_name );
+    }
     $slug = str_replace( '/', '-', $name );
     wp_register_style(
       $slug . '-editor',
@@ -85,8 +148,18 @@ function gutenfactory_run() {
       'editor_script' => 'gutenfactory-editor',
       'editor_style'  => $slug . '-editor',
       'style'         => $slug,
+      'attributes'      => gutenfactory_attributes( $attributes['fields'] ),
+      'render_callback' => $attributes['callback'],
     ) );
+    $gutenfactory_blocks[ $name ] = $attributes;
   }
   wp_localize_script( 'gutenfactory-editor', 'gutenfactory_blocks', $gutenfactory_blocks );
+}
+function gutenfactory_attributes( $attributes ) {
+  $result = [];
+  foreach ( $attributes as $key => $attribute ) {
+    $result[ $key ] = [ 'type' => $attribute['data_type'] ];
+  }
+  return $result;
 }
 add_action( 'init', 'gutenfactory_run' );
